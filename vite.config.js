5f7008f8c2e0bfccce0cc52558b25384359de91e
Gorka <gorkacarmonapino@gmail.com>
2025-03-04 19:03:36 +0100

    Produccion 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/<Recordatorio1>/' : '/', // Reemplaza <REPO_NAME> con el nombre de tu repositorio
+  base: process.env.NODE_ENV === 'production' ? '/Recordatorio1/' : '/',
 });
